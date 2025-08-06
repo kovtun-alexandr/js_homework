e9298ef40b4910fcde38c21b70d02515882d2e56
kovtun-alexandr <kovtunalexander.84@gmail.com>
2025-08-06 15:56:14 +0300

    add task #10 to homework #5

--- lesson_05/script/task_10.js
+++ lesson_05/script/task_10.js
@@ -4,7 +4,19 @@
 // числами.
 
 if (confirm('Почати тестування?')) {
+    const firstNumber = parseInt(prompt('Введіть перше число:', '5'))
+    const secondNumber = parseInt(prompt('Введіть друге число:', '25'))
+    let sumNumber = 0
 
+    if (firstNumber < secondNumber) {
+        for (let i = firstNumber; i <= secondNumber; i++) {
+            if (i % 2 === 1)
+                sumNumber += i
+        }
+        document.write(`Суму всіх непарних чисел від ${firstNumber} до ${secondNumber} дорівнює ${sumNumber}`)
+    } else {
+        document.write('Ви зробили помилку, Спробуйте ще раз!')
+    }
 } else {
     alert(`Нажаль ви відмовились від теста 😞`)
 }
