9a1465c8365b8daa7c951fba6b280e187f41b944
kovtun-alexandr <kovtunalexander.84@gmail.com>
2025-08-06 15:04:15 +0300

    add task #9 to homework #5

--- lesson_05/script/task_9.js
+++ lesson_05/script/task_9.js
@@ -4,7 +4,24 @@
 // (використати confirm).
 
 if (confirm('Почати тестування?')) {
+    const guessNumber = parseInt(prompt('Загадайте число:', '4'))
+    let searchNumber = 5,
+        attemptsNumber = 0
 
+    while (guessNumber !== searchNumber) {
+        const userRespon = confirm(`Ваше число більше за ${searchNumber}?`)
+        if (userRespon) {
+            searchNumber += 2
+            attemptsNumber++
+        } else {
+            searchNumber -= 1
+            attemptsNumber++
+        }
+    }
+
+    if (guessNumber === searchNumber) {
+        document.write(`Вітаю компьютер впорався і вгадав ваше число ${guessNumber} Йому знадобилось ${attemptsNumber} спроб`)
+    }
 } else {
     alert(`Нажаль ви відмовились від теста 😞`)
 }
